86db13ab4e669f2355dba2c3e6063cf731dacc16
Charlie Person <charlie.person@hyperisland.se>
2015-11-25 12:28:47 +0000

    CH14 RSpec passed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,21 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  def initialize
+    @count = 0
+    @max_capacity = 20
+  end
+
+  attr_reader :bikes
 
   def release_bike
+    raise "No bikes bucko" unless @count > 0        # <= guard clause
     Bike.new
   end
 
   def dock(bike)
-    @bike = bike
+    raise "Max is 20" if @count >= @max_capacity
+    @count +=1
+    @bikes = bike
   end
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,11 +5,13 @@ describe DockingStation do
 
     it { is_expected.to respond_to(:dock).with(1).argument }
 
-    it { is_expected.to respond_to(:bike) }
+    it { is_expected.to respond_to(:bikes) }
 
     it 'releases working bikes' do
-      bike = subject.release_bike
-      expect(bike).to be_working
+      bike = Bike.new
+      subject.dock(bike)
+      # bike = subject.release_bike
+      expect(subject.release_bike).to be_working
     end
 
     it 'docks something' do
@@ -20,8 +22,24 @@ describe DockingStation do
     it 'returns docked bikes' do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.bike).to eq bike
+      expect(subject.bikes).to eq bike
     end
+
+    it 'raise error when one tries to release bikes when there are none' do
+      expect{subject.release_bike}.to raise_error("No bikes bucko")
+    end
+
+    # it "raise error when docking station is full" do
+    #   bike = Bike.new
+    #   subject.dock(bike) # fill the dock
+    #   expect{subject.dock(bike)}.to raise_error("No space mate- dock is full")
+    # end
+
+    it "Default capacity of docking station = 20" do
+      20.times {subject.dock Bike.new}
+      expect{subject.dock(Bike.new)}.to raise_error("Max is 20")
+    end
+
 end
 
 
